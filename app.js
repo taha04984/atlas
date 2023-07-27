0aa723b6e5444f65914b45928bf186340c8318d4
taha04984 <92213215+taha04984@users.noreply.github.com>
2023-07-27 15:31:34 +0530

    Delete app.js

--- app.js
+++
@@ -1,7 +0,0 @@
-let menu = document.querySelector('.menu');
-let navlinks = document.querySelector('.navlinks');
-
-menu.addEventListener('click', ()=>{
-    // navlinks.style.left = "0%";
-    navlinks.classList.toggle('left');
-})
